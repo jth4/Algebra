b1e017ab0e0521d110f1162d5d6780fe06f33c62
jth4 <jth4@spiderman.local>
2018-09-17 10:59:40 -0700

    Initial Commit

---
+++ Algebra/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Algebra
+//
+//  Created by jth4 on 9/17/18.
+//  Copyright © 2018 jth4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
